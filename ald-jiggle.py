c274588c156342928e39d50b99865ad82f7d8657
Marshall Whittaker <marshall@oxagast.org>
2023-11-02 11:13:05 -0400

    Implimented a timer and corrected correct handling of it.

--- ald-jiggle.py
+++ ald-jiggle.py
@@ -8,15 +8,19 @@ import time
 import re
 import uuid
 import multiprocessing
+import getpass
 from bthid import BluetoothHIDService
 from dbus.mainloop.glib import DBusGMainLoop
+
 sys.tracebacklimit = 0  # this removes the traceback
-start_time = time.time()
 waiting = 0
 attackmac = (hex(uuid.getnode()+4).lstrip("0x").zfill(2).upper()) # gotta add 4 to get to the HID ctl otherwise proto wrong
 attackmac = ':'.join(attackmac[i:i+2] for i in range(0, len(attackmac), 2))  # this just adds the : every 2 chars
-print("[?] Jiggalo by oxagast")
+print("[?] AntiLockDown by oxagast")
 print("[?] Inhibits screensavers by pressing innoculous keys over bluetooth.")
+if getpass.getuser() != "root":
+    sys.stderr.write("[x] You need to run this program as root!\n")
+    sys.exit(1)
 print("[*] Attacking MAC detected as: %s" % attackmac)
 def macr(send_call_back):
     def fkeys():
@@ -29,23 +33,22 @@ def macr(send_call_back):
             send_call_back(bytes(kstate))
             time.sleep(15) # every 15 seconds so we're well within the screensaver timer
     def ani():
-        waiting = 1;
+        waiting = 1
         idx = 0
         while waiting:
             animation = "|/-\\"  # we iterate over this
             print(animation[idx % len(animation)], end='\r') # \r repositions the cursor at start
             idx+=1
             time.sleep(0.1)
-
     print("[!] Inhibiting lock screen on remote computer!")
-    jiggle = 1
+    global st
+    st = time.time()
     anip = multiprocessing.Process(target=ani) # the spinning animation thread
     jigp = multiprocessing.Process(target=fkeys) # pressing keys thread
     jigp.start()
     anip.start()
     jigp.join()
     anip.join()
-
 if __name__ == '__main__':
     DBusGMainLoop(set_as_default=True)
     srec = open("sdpr.xml").read()
@@ -53,5 +56,7 @@ if __name__ == '__main__':
         bthid_srv = BluetoothHIDService(srec, attackmac)
         macr(bthid_srv.send)
     finally:
-        print("\n[*] Jiggled for %s seconds." % (time.time() - start_time))
-        print("[x] Jiggling stopped.")
+        if 'st' in globals():
+            print("\n[*] Jiggled for %s seconds." % (time.time() - st))
+        print("[x] Stopped.")
+        sys.exit(0)
